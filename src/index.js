c8787b586a398dd698f699560aa53f4520519154
SomayaAbdulkareem <98326369+SomayaAbdulkareem@users.noreply.github.com>
2022-02-06 21:23:22 +0300

    intialize the server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 const express =require('express');
 const mongoose =require('mongoose');
+
+const app = express()
+
+
+
+
+app.listen(4000,(req,res)=>{
+  console.log("The server is running on 4000 port ");
+})
