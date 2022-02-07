9c90ccd33bd9a1507e00dcf9f6ce4da3f739e146
Reem <98323193+reemy0@users.noreply.github.com>
2022-02-07 12:13:07 +0300

    Update student.js

--- src/models/student.js
+++ src/models/student.js
@@ -1,10 +1,11 @@
-const { Mongoose } = require("mongoose");
-Mongoose.connect('mongodb://localhost/Teach_ME')
+const mongoose  = require("mongoose");
+mongoose.connect('mongodb://localhost/Teach_ME')
 .then(()=> console.log('connection done'))
 .catch((err)=> console.log('connection failed'))
 
-const schema = new mongoose.schema({
-    Studentid : mongoose.schema.Objectid,
-    rev_content:String,
-    data:Date
+const schema = new mongoose.Schema({
+    user : mongoose.Schema.Types.Objectid,
+    favorit_list:
+   [mongoose.Schema.Types.Mixed],
+    profile:mongoose.Schema.Types.Mixed
 })
