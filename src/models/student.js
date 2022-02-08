f2fe84e248cc2de8a5cf7c138600d37a78eaf530
Reem <98323193+reemy0@users.noreply.github.com>
2022-02-08 18:22:03 +0300

    update the student

--- src/models/student.js
+++ src/models/student.js
@@ -3,7 +3,7 @@ mongoose.connect('mongodb://localhost/Teach_ME')
 .then(()=> console.log('connection done'))
 .catch((err)=> console.log('connection failed'))
 
-const schema = new mongoose.Schema({
+const schema = mongoose.Schema({
     user : mongoose.Schema.Types.Objectid,
     favorit_list:
    [mongoose.Schema.Types.Mixed],
