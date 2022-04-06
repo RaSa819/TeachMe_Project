bfc972551e38496ae2494152237e35a47e292411
Reem <98323193+reemy0@users.noreply.github.com>
2022-04-06 11:00:40 +0300

    u

--- endpoint/src/pages/home/homePage.js
+++ endpoint/src/pages/home/homePage.js
@@ -13,48 +13,14 @@ window.addEventListener('scroll', function () {
 })
 
 export default function HomePage() {
-<<<<<<< HEAD
-  return (
-    <div>
-   
-      <Topbar/>
-  
-      <section class="banner">
-      <div class="container">
-        <div class='content'>
-    <h2>Save time and connect to the best tutors.</h2>
-    <h4>this is a platform to bring students and tutors 
-      <br></br>together and make learning easy and affordable </h4>
-      <hr class='homeHR'></hr>
-    <button type="button" class="btn btn-light ">Get Started</button>
-        </div>
-         
-     
-</div>
-<a href='#section-2'><div class='scroll-down'></div></a>
-      </section>
-      <section class='section-2' id='section-2'>
-        <div class='container'>
-          <h1>What is Teach me ?</h1>
-          <h4>a Profitable educational platform 
-            <br></br>provides service for both students and tutors  </h4>
-        </div>
-        <a href='#section-3'><div class='scroll-down'></div></a>
-      </section>
 
-      <section class='section-3' id='section-3'>
-        <div class='container'>
-          <h1>Are you looking for Information Immediately ?</h1>
-          <h4>You came to the right place! <br></br>
-just make a request and your request will be handed
-to the best tutors ! immediately</h4>
-    <a href="/Signup"  type="button" class="btn btn-light ">Join as Student</a>
-=======
+ 
+
     return (
         <div>
 
             <Topbar/>
->>>>>>> f06525efc69e8673516ea97ccf139a4a2954a949
+
 
             <section class="banner">
                 <div class="container">
@@ -101,21 +67,14 @@ to the best tutors ! immediately</h4>
                 </a>
             </section>
 
-            <section class='section-4' id='section-4'>
-                <div class='container'>
-                    <h1>Are you a Specialized  Tutor Who’s Looking for Extra Income Resources?</h1>
-                    <h4>we are looking for tutors with an experience and passion about teaching !
-                    </h4>
-                    <button type="button" class="btn btn-light tutorJoin ">Join as Tutor</button>
-
-<<<<<<< HEAD
+            
       <section class='section-4' id='section-4'>
         <div class='container'>
           <h1>Are you a Specialized  Tutor Who’s Looking for Extra Income Resources?</h1>
           <h4>we are looking for tutors with an experience and passion about teaching ! </h4>
-   <a href="/Signup"   class="btn btn-light tutorJoin ">Join as Tutor</a>
-=======
->>>>>>> f06525efc69e8673516ea97ccf139a4a2954a949
+   <button type="button" class="btn btn-light tutorJoin ">Join as Tutor</button>
+
+ 
 
                 </div>
 
