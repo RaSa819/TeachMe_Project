3d64952c385860c4b22d59cc8c47b2538e475156
Olexandr <olexandrlyap@gmail.com>
2022-04-29 11:05:26 +0200

    Extract session logic into a hook, add enable/disable buttons

---
+++ endpoint/src/hooks/useRTCSession.js
@@ -0,0 +1,109 @@
+import { useCallback, useContext, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { SocketContext } from "../Socket";
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+export default function useRTCSession() {
+  const { sessionID } = useParams();
+  const socket = useContext(SocketContext);
+
+  const peerConnection = useRef(new RTCPeerConnection(servers));
+  const localStream = useRef();
+  const remoteStream = useRef(new MediaStream());
+  const videoRef = useRef();
+  useEffect(() => {
+    (async () => {
+        localStream.current = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: { width: window.innerWidth, height: window.innerHeight },
+        });
+        localStream.current.getTracks().forEach((track) => {
+            peerConnection.current.addTrack(track, localStream.current);
+        });
+        
+        peerConnection.current.ontrack = (event) => {
+            event.streams[0].getTracks().forEach((track) => {
+                remoteStream.current.addTrack(track);
+            });
+        };
+
+        videoRef.current.srcObject = remoteStream.current;
+
+        const type = Number(localStorage.getItem('type'));
+
+        if (type === 1) {
+            const offerDescription = await peerConnection.current.createOffer();
+            await peerConnection.current.setLocalDescription(offerDescription);
+
+            const callOffer = {
+                sdp: offerDescription.sdp,
+                type: offerDescription.type,
+            };
+
+            socket.emit('tutor-call-offer', { sessionID, callOffer });
+
+            socket.on('webrtc-answer', (answer) => {
+                const answerDescription = new RTCSessionDescription(answer);
+                peerConnection.current.setRemoteDescription(answerDescription);
+            });
+        } else {
+            socket.on('webrtc-offer', async (offer) => {
+                const offerDescription = new RTCSessionDescription(offer);
+                await peerConnection.current.setRemoteDescription(offerDescription);
+
+                const answerDescription = await peerConnection.current.createAnswer();
+                await peerConnection.current.setLocalDescription(answerDescription);
+                const callAnswer = {
+                    sdp: answerDescription.sdp,
+                    type: answerDescription.type,
+                };
+                socket.emit('student-call-answer', { sessionID, callAnswer })
+            });
+        }
+
+        const iceCandidates = [];
+        peerConnection.current.onicecandidate = ({ candidate }) => {
+            if (candidate) iceCandidates.push(candidate.toJSON());
+        };
+
+        peerConnection.current.onicegatheringstatechange = () => {
+            if (peerConnection.current.iceGatheringState === 'complete') {
+                socket.emit('ice-candidates-update', { candidates: iceCandidates, sessionID });
+            }
+        };
+
+        socket.on('ice-candidates-update', (candidates) => {
+            candidates.forEach((candidate) => {
+                candidate.usernameFragment = null;
+                peerConnection.current.addIceCandidate(new RTCIceCandidate(candidate));
+            });
+        });
+    })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const toggleCamera = useCallback((isEnabled) => {
+    localStream.current.getVideoTracks()[0].enabled = isEnabled;
+  }, []);
+
+  const toggleMic = useCallback((isEnabled) => {
+    localStream.current.getAudioTracks()[0].enabled = isEnabled;
+  }, []);
+
+  return {
+    peerConnection: peerConnection.current,
+    localStream: localStream.current,
+    remoteStream: remoteStream.current,
+    videoRef,
+    toggleCamera,
+    toggleMic,
+  };
+}
--- endpoint/src/pages/Session.js
+++ endpoint/src/pages/Session.js
@@ -1,94 +1,33 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
-import { SocketContext } from '../Socket';
-
-const servers = {
-    iceServers: [
-      {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-      },
-    ],
-    iceCandidatePoolSize: 10,
-};
+import React, { useCallback, useState } from 'react';
+import useRTCSession from '../hooks/useRTCSession';
 
 const Session = () => {
-    const { sessionID } = useParams();
-    const socket = useContext(SocketContext);
+    const { videoRef, toggleCamera, toggleMic } = useRTCSession();
+    const [isCameraEnabled, setIsCameraEnabled] = useState(true);
+    const [isMicEnabled, setIsMicEnabled] = useState(true);
+    const handleCameraToggle = useCallback(() => {
+        setIsCameraEnabled((isEnabled) => {
+            toggleCamera(!isEnabled);
+            return !isEnabled;
+        })
+    }, [toggleCamera]);
+    const handleMicToggle = useCallback(() => {
+        setIsMicEnabled((isEnabled) => {
+            toggleMic(!isEnabled);
+            return !isEnabled;
+        });
+    }, [toggleMic]);
 
-    const peerConnection = useRef(new RTCPeerConnection(servers));
-    const localStream = useRef();
-    const remoteStream = useRef(new MediaStream());
-    const videoRef = useRef();
-    useEffect(() => {
-        (async () => {
-            localStream.current = await navigator.mediaDevices.getUserMedia({
-              audio: true,
-              video: { width: window.innerWidth, height: window.innerHeight },
-            });
-            localStream.current.getTracks().forEach((track) => {
-                peerConnection.current.addTrack(track, localStream.current);
-            });
-            
-            peerConnection.current.ontrack = (event) => {
-                event.streams[0].getTracks().forEach((track) => {
-                    remoteStream.current.addTrack(track);
-                });
-            };
-
-            videoRef.current.srcObject = remoteStream.current;
-
-            const type = Number(localStorage.getItem('type'));
-
-            if (type === 1) {
-                const offerDescription = await peerConnection.current.createOffer();
-                await peerConnection.current.setLocalDescription(offerDescription);
-
-                const callOffer = {
-                    sdp: offerDescription.sdp,
-                    type: offerDescription.type,
-                };
-
-                socket.emit('tutor-call-offer', { sessionID, callOffer });
-
-                socket.on('webrtc-answer', (answer) => {
-                    const answerDescription = new RTCSessionDescription(answer);
-                    peerConnection.current.setRemoteDescription(answerDescription);
-                });
-            } else {
-                socket.on('webrtc-offer', async (offer) => {
-                    const offerDescription = new RTCSessionDescription(offer);
-                    await peerConnection.current.setRemoteDescription(offerDescription);
-
-                    const answerDescription = await peerConnection.current.createAnswer();
-                    await peerConnection.current.setLocalDescription(answerDescription);
-                    const callAnswer = {
-                        sdp: answerDescription.sdp,
-                        type: answerDescription.type,
-                    };
-                    socket.emit('student-call-answer', { sessionID, callAnswer })
-                });
-            }
-
-            const iceCandidates = [];
-            peerConnection.current.onicecandidate = ({ candidate }) => {
-                if (candidate) iceCandidates.push(candidate.toJSON());
-            };
-
-            peerConnection.current.onicegatheringstatechange = () => {
-                if (peerConnection.current.iceGatheringState === 'complete') {
-                    socket.emit('ice-candidates-update', { candidates: iceCandidates, sessionID });
-                }
-            };
-
-            socket.on('ice-candidates-update', (candidates) => {
-                candidates.forEach((candidate) => {
-                    peerConnection.current.addIceCandidate(new RTCIceCandidate(candidate));
-                });
-            });
-        })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return <video ref={videoRef} style={{ width: window.innerWidth, height: window.innerHeight }} />;
+    return (
+      <>
+        <video
+            ref={videoRef}
+            style={{ width: window.innerWidth, height: window.innerHeight }}
+            autoPlay
+        />
+        <button onClick={handleMicToggle}>{isMicEnabled ? 'Disable Mic' : 'Enable Mic'}</button>
+        <button onClick={handleCameraToggle}>{isCameraEnabled ? 'Disable Camera' : 'Enable Camera'}</button>
+      </>
+    );
 }
 export default Session;
