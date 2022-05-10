7dddf49f2432b42cec71ebda56d58aea533befa9
Zooz <zahraa95emad@gmail.com>
2022-05-10 12:40:00 +0200

    student dashboard

--- endpoint/src/App.js
+++ endpoint/src/App.js
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 import {SocketProvider} from "./Socket";
 
 import Card from "./components/Card";
-
+import StudentDashboard from "./pages/StudentDashboard";
 import Login from "./pages/Login";
 import Users from "./pages/Users";
 import Depts from "./pages/Depts";
@@ -149,6 +149,12 @@ export default function App() {
 
                                 <Route path="/Login"
                                     element={<Login/>}/>
+
+
+                                        <Route path="/StudentDashboard"
+                                    element={ <StudentDashboard/>}/>
+
+
                                 <Route path="/Signup"
                                     element={<SingupWithValidation/>}/>
 
